32e9fbbe175ea0f993619f2887f9a0b556c1903b
iphonesector-hash <iphone.sector@gmail.com>
2026-06-26 00:12:32 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -2,23 +2,21 @@ import sys
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+# مسیر اصلی پروژه
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+
+# خواندن .env از ریشه پروژه
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 from telegram.ext import (
     Application,
     CommandHandler,
+    MessageHandler,
+    filters
 )
 
-# اضافه کردن مسیر ریشه برای import ها
-current_dir = os.path.dirname(os.path.abspath(__file__))
-parent_dir = os.path.dirname(current_dir)
-
-if parent_dir not in sys.path:
-    sys.path.insert(0, parent_dir)
-
-
 from bot.database.session import init_db
-
 from bot.handlers.start import start_handler
 from bot.handlers.errors import error_handler
 
@@ -36,24 +34,22 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 def main():
 
     if not BOT_TOKEN:
-        print("❌ BOT_TOKEN پیدا نشد. فایل .env را چک کن.")
+        print("❌ BOT_TOKEN پیدا نشد. فایل .env را چک کنید.")
         return
 
-
+    # دیتابیس
     init_db()
-
 
     app = Application.builder().token(BOT_TOKEN).build()
 
 
-    # گروه 0: ثبت نام کاربر
+    # گروه 0 - ثبت کاربران
     for handler in get_registration_handlers():
         app.add_handler(handler, group=0)
 
 
-    # گروه 1: دستورات
+    # گروه 1 - دستورات
     app.add_handler(start_handler, group=1)
-
 
     for handler in get_panel_handlers():
         app.add_handler(handler, group=1)
@@ -74,22 +70,20 @@ def main():
 
 
 
-    # خوشامد
+    # گروه 2 - خوش آمد
     for handler in get_welcome_handlers():
         app.add_handler(handler, group=2)
 
 
-    # ضد اسپم
+    # گروه 3 - ضد اسپم
     for handler in get_antispam_handlers():
         app.add_handler(handler, group=3)
-
 
 
     app.add_error_handler(error_handler)
 
 
     print("✅ SectorBot روشن شد... منتظر پیام هستم.")
-
 
     app.run_polling()
 
