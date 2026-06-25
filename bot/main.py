2ab2704acb5092abca82bf827af83d4edd6d7ca2
iphonesector-hash <iphone.sector@gmail.com>
2026-06-26 00:09:19 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,24 @@
 import sys
 import os
-from telegram.ext import Application, CommandHandler, MessageHandler, filters
+from dotenv import load_dotenv
 
-# اضافه کردن مسیر ریشه به sys.path برای کارکرد صحیح importهای bot.xxx
+load_dotenv()
+
+from telegram.ext import (
+    Application,
+    CommandHandler,
+)
+
+# اضافه کردن مسیر ریشه برای import ها
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
+
 if parent_dir not in sys.path:
     sys.path.insert(0, parent_dir)
 
+
 from bot.database.session import init_db
+
 from bot.handlers.start import start_handler
 from bot.handlers.errors import error_handler
 
@@ -19,28 +29,35 @@ from bot.modules.antispam import get_antispam_handlers
 from bot.modules.profile import get_profile_handlers
 from bot.modules.registration import get_registration_handlers
 
-# استفاده از متغیر محیطی برای امنیت بیشتر
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8819957944:AAFVCeFQ3RXPImvhF3jjL1D418xIg9B9JLs")
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 
 def main():
-    # Initialize Database
+
+    if not BOT_TOKEN:
+        print("❌ BOT_TOKEN پیدا نشد. فایل .env را چک کن.")
+        return
+
+
     init_db()
 
-    if BOT_TOKEN == "8819957944:AAFVCeFQ3RXPImvhF3jjL1D418xIg9B9JLs":
-        print("⚠️ Warning: Using default bot token. Set BOT_TOKEN environment variable for production.")
 
     app = Application.builder().token(BOT_TOKEN).build()
 
-    # گروه 0: ثبت‌نام (Registration) - باید اولین چیزی باشد که اجرا می‌شود
+
+    # گروه 0: ثبت نام کاربر
     for handler in get_registration_handlers():
         app.add_handler(handler, group=0)
 
-    # گروه 1: تمام دستورات (Commands)
+
+    # گروه 1: دستورات
     app.add_handler(start_handler, group=1)
+
 
     for handler in get_panel_handlers():
         app.add_handler(handler, group=1)
+
 
     for handler in get_profile_handlers():
         if isinstance(handler, CommandHandler):
@@ -48,26 +65,34 @@ def main():
         else:
             app.add_handler(handler, group=5)
 
+
     for handler in get_lock_handlers():
         if isinstance(handler, CommandHandler):
             app.add_handler(handler, group=1)
         else:
             app.add_handler(handler, group=4)
 
-    # گروه 2: خوشامدگویی
+
+
+    # خوشامد
     for handler in get_welcome_handlers():
         app.add_handler(handler, group=2)
 
-    # گروه 3: ضد اسپم
+
+    # ضد اسپم
     for handler in get_antispam_handlers():
         app.add_handler(handler, group=3)
 
-    # خطاها
+
+
     app.add_error_handler(error_handler)
 
-    print("ربات SectorBot روشن شد... منتظر پیام هستم.")
+
+    print("✅ SectorBot روشن شد... منتظر پیام هستم.")
+
 
     app.run_polling()
+
 
 
 if __name__ == "__main__":
