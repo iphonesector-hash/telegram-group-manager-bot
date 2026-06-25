b043be3a0a9c63d968342affeeee2bbf510c6bdb
iphonesector-hash <iphone.sector@gmail.com>
2026-06-25 18:02:15 +0330

    Update start.py

--- bot/bot/handlers/start.py
+++ bot/bot/handlers/start.py
@@ -1,1 +1,8 @@
+from telegram import Update
+from telegram.ext import ContextTypes, CommandHandler
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ربات مدیریت گروه فعال شد ✨")
+
+start_handler = CommandHandler("start", start)
 
