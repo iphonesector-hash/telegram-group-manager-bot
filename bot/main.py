d3513825c210b5397de9274748e5245e93b924cf
iphonesector-hash <iphone.sector@gmail.com>
2026-06-25 18:35:59 +0330

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,17 @@
 from telegram.ext import Application
+
 from bot.handlers.start import start_handler
 from bot.handlers.errors import error_handler
-from bot.modules.locks import get_handlers as get_lock_handlers
+
 from bot.modules.panel import get_panel_handlers
+from bot.modules.locks import get_handlers as get_lock_handlers
 from bot.modules.welcome import get_welcome_handlers
 from bot.modules.antispam import get_antispam_handlers
 from bot.modules.profile import get_profile_handlers
 
+
 BOT_TOKEN = "8819957944:AAFVCeFQ3RXPImvhF3jjL1D418xIg9B9JLs"
+
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
@@ -15,12 +19,12 @@ def main():
     # /start
     app.add_handler(start_handler)
 
-    # قفل‌ها
-    for handler in get_lock_handlers():
+    # پنل مدیریت (باید قبل از filters.ALL باشد)
+    for handler in get_panel_handlers():
         app.add_handler(handler)
 
-    # پنل مدیریت
-    for handler in get_panel_handlers():
+    # قفل‌ها
+    for handler in get_lock_handlers():
         app.add_handler(handler)
 
     # خوشامدگویی
@@ -39,7 +43,9 @@ def main():
     app.add_error_handler(error_handler)
 
     print("ربات روشن شد... منتظر پیام هستم.")
+
     app.run_polling()
+
 
 if __name__ == "__main__":
     main()
