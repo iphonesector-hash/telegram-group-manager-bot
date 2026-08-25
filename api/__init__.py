8f13195480f9bc5cbd17ba2d09992fa0c6f70b19
iphonesector-hash <iphone.sector@gmail.com>
2026-08-25 15:12:25 +0330

    perf(api): enable coherent Sector mutation responses

--- api/__init__.py
+++ api/__init__.py
@@ -9,6 +9,7 @@ from api import sector_v2_extra_routes  # noqa: F401,E402
 from api import sector_meta_routes  # noqa: F401,E402
 from bot.services import sector_mission_personalization  # noqa: F401,E402
 from bot.services import sector_relationship_memory  # noqa: F401,E402
+from bot.services import sector_coherence  # noqa: F401,E402
 from api import sector_reminder_routes  # noqa: F401,E402
 from api import sector_action_intent_routes  # noqa: F401,E402
 from api import sector_diagnostics_routes  # noqa: F401,E402
