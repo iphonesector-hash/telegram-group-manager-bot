45f99161de54d3abd8446fd5b1ae0a59991492bf
iphonesector-hash <iphone.sector@gmail.com>
2026-08-25 14:24:33 +0330

    feat(api): enable personalized Sector missions

--- api/__init__.py
+++ api/__init__.py
@@ -7,5 +7,6 @@ from bot.services import sector_locale  # noqa: F401,E402
 from api import sector_v2_routes  # noqa: F401,E402
 from api import sector_v2_extra_routes  # noqa: F401,E402
 from api import sector_meta_routes  # noqa: F401,E402
+from bot.services import sector_mission_personalization  # noqa: F401,E402
 from api import sector_reminder_routes  # noqa: F401,E402
 from api import sector_action_intent_routes  # noqa: F401,E402
